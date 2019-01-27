e801b45ecc6df622442ced3419395a1cfb0c03df
kedanco <kelvinchowjunjie120@hotmail.com>
2019-01-28 04:38:27 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			quote: "",
+			author: "",
+			tweetStr: "http://twitter.com/intent/tweet?text=",
+			fsize: "36px"
+		};
+	}
+
+	newQuote() {
+		fetch("https://talaikis.com/api/quotes/random/")
+			.then(data => data.json())
+			.then(data => {
+				let fsize = data.quote.length > 150 ? "24px" : "32px";
+				this.setState({
+					quote: data.quote,
+					author: data.author,
+					tweetStr: this.state.tweetStr + data.quote + " - " + data.author,
+					fsize: fsize
+				});
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	}
+
+	componentDidMount() {
+		this.newQuote();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div id="quote-box">
+					<h1 id="title">Random Quote Machine</h1>
+					<h3 id="text" style={{ fontSize: this.state.fsize }}>
+						{this.state.quote}
+					</h3>
+					<div id="author">
+						<div id="dash">{""}</div>
+						{this.state.author}
+					</div>
+					<div className="buttons">
+						<button id="new-quote" onClick={() => this.newQuote()}>
+							New Quote
+						</button>
+						<a href={this.state.tweetStr} target="_blank">
+							<button id="tweet-quote">Tweet Quote</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
